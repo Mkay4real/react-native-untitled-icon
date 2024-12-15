e6ae356265021b21d1447570ca88720c70a476be
Mukhtar Oyelayo <mukhtaroyelayo@gmail.com>
2024-12-15 12:35:01 +0100

    chore: implement auto completion on icon names

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,25 @@
-declare interface UntitledIconProps {
-  name: string;
+import { SvgProps } from "react-native-svg";
+type DefaultName = keyof typeof import('./icons');
+
+type ToKebabCase<S extends string> =
+  S extends `${infer First}${infer Rest}`
+  ? Rest extends `${infer Digit}${infer R}` // Check if next char is a digit
+  ? Digit extends '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+  ? `${Lowercase<First>}-${Digit}${ToKebabCase<R>}`
+  : Rest extends Uncapitalize<Rest>
+  ? `${Lowercase<First>}${ToKebabCase<Rest>}`
+  : `${Lowercase<First>}-${ToKebabCase<Rest>}`
+  : `${Lowercase<First>}`
+  : S;
+
+type IconName = {
+  [K in DefaultName]: ToKebabCase<K extends `Svg${infer Name}` ? Name : K>;
+}[DefaultName];
+
+type Name = ToKebabCase<DefaultName>;
+
+declare interface UntitledIconProps extends Omit<SvgProps, 'width' | 'height'> {
+  name: IconName;
   size?: number | string;
   color?: string;
   [key: string]: any;
